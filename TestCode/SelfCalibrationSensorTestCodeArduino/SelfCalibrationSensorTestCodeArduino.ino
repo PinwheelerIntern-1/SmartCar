9b780d8c0f8b14ae136aaec71d3165e5f430945f
Prateek <prateek.ideafires@gmail.com>
2021-11-23 12:01:27 +0530

    Updated with Calibrate and Line Mode

--- TestCode/SelfCalibrationSensorTestCodeArduino/SelfCalibrationSensorTestCodeArduino.ino
+++ TestCode/SelfCalibrationSensorTestCodeArduino/SelfCalibrationSensorTestCodeArduino.ino
@@ -1,11 +1,17 @@
 
+#include <SoftwareSerial.h>
 
-  //(Rx, Tx), connect Tx of sensor board to pin 11 and Rx of sensor board to pin 12
+#define Sensor_Tx  //to any pin on arduino which will serve as Rx pin 
+#define Sensor_Rx  //to any pin on arduino which will serve as Tx pin
+#define Sensor_calibration //tp the digital pin which will be used to calibrate sensor
+SoftwareSerial Sensor(Sensor_Tx, Sensor_Rx), //connect Tx of sensor board to Rx pin of arduino and Rx of sensor board to Tx pin of arduino
 String data="";
 void setup() {
+  pinMode(Sensor_calibration, OUTPUT);
+  digitalWrite(Sensor_calibration, HIGH);
   // put your setup code here, to run once:
 Serial.begin(9600);
-
+Sensor.begin(9600);
 }
 
 void loop() {
@@ -21,4 +27,22 @@ void loop() {
   }
 
 Serial.println(data);
+}
+//call this function to calibrate
+void Calibrate(){
+  digitalWrite(Sensor_calibration, LOW);
+  delay(100);
+  digitalWrite(Sensor_calibration, HIGH);
+}
+
+//call this function to change mode
+void ChangeLineMode(){
+  digitalWrite(Sensor_calibration, LOW);
+  delay(100);
+  digitalWrite(Sensor_calibration, HIGH);
+  delay(100);
+  digitalWrite(Sensor_calibration, LOW);
+  delay(100);
+  digitalWrite(Sensor_calibration, HIGH);
+}
 }
